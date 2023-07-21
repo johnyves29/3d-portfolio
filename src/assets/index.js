5b72d1fe8bf0bfe5c1cc45863c0e22dfa42895c4
DESKTOP-0F23VUH\johny <johnyvesmarin@gmail.com>
2023-07-21 21:27:28 +0800

    minify assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,21 +8,21 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import link from './link.png'
 
-import css from "./tech/css.png";
+import css from "./tech/css.webp";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
+import html from "./tech/html.webp";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
+import mongodb from "./tech/mongodb.webp";
+import nodejs from "./tech/nodejs.webp";
+import reactjs from "./tech/reactjs.webp";
 import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
+import tailwind from "./tech/tailwind.webp";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import bootstrap from "./tech/bootstrap.png"
-import githubsign from "./tech/githubsign.png";
+import bootstrap from "./tech/bootstrap.webp"
+import githubsign from "./tech/githubsign.webp";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
@@ -34,22 +34,22 @@ import gallery from "./company/gallery.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-import wilmar from './wilmar.png';
-import oldportfolio from './oldportfolio.png';
-import aps from './aps.png';
-import erp from './erp.png';
-import mnk from './mnk.png';
-import etc23 from './etc23.png';
-import onlineclassroom from './onlineclassroom.png';
-import phdaily from './phdaily.png';
+import wilmar from './wilmar.webp';
+import oldportfolio from './oldportfolio.webp';
+import aps from './aps.webp';
+import erp from './erp.webp';
+import mnk from './mnk.webp';
+import etc23 from './etc23.webp';
+import onlineclassroom from './onlineclassroom.webp';
+import phdaily from './phdaily.webp';
 
-import fcc from './certificates/fcc.png';
-import solearnhmtl from './certificates/sololearnhtml.jpg';
-import solearnjs from './certificates/sololearnjs.jpg';
-import awesomeday from './certificates/awesomeday.jpg';
-import udemywp from './certificates/udemywp.jpg';
-import yrs from './certificates/yrs.jpg';
-import eth from './certificates/eth.png';
+import fcc from './certificates/fcc.webp';
+import solearnhmtl from './certificates/sololearnhtml.webp';
+import solearnjs from './certificates/sololearnjs.webp';
+import awesomeday from './certificates/awesomeday.webp';
+import udemywp from './certificates/udemywp.webp';
+import yrs from './certificates/yrs.webp';
+import eth from './certificates/eth.webp';
 
 export {
   logo,
